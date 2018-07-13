18c201045b31b56c462738dac0732d446b5534c6
Wyatt Barnes <wyattbarnes@protonmail.com>
2018-07-13 16:40:45 -0500

    Basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomModalComponent } from './custom-modal/custom-modal.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/custom-modal/custom-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-modal',
+  templateUrl: './custom-modal.component.html',
+  styleUrls: ['./custom-modal.component.css']
+})
+export class CustomModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
