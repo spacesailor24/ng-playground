eeeb94d23d349f010b713322cdcd372240bbb88d
Wyatt Barnes <wyattbarnes@protonmail.com>
2018-07-16 16:34:00 -0500

    Adjust module imports accordingly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
-import { ModalsComponent } from './modules/home/pages/modals/modals.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { CustomStandardModalComponent } from './shared/components/custom-standard-modal/custom-standard-modal.component';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'modals', component: ModalsComponent },
-  { path: '**', redirectTo: '/home' },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ModalsComponent,
-    NavbarComponent,
-    CustomStandardModalComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // debugging purposes only
-    ),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreRoutingModule } from './core-routing.module';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { WelcomeComponent } from '../features/welcome/welcome.component';
+import { ModalsComponent } from '../features/playsets/modals/modals.component';
+import { HeroHeaderComponent } from '../shared/components/hero-header/hero-header.component';
+import { SharedModule } from '../shared/shared.module';
+
 @NgModule({
+  declarations: [
+    NavbarComponent,
+    WelcomeComponent,
+    ModalsComponent,
+    HeroHeaderComponent,
+],
   imports: [
-    CommonModule
-  ],
-  declarations: []
+    CommonModule,
+    CoreRoutingModule,
+    SharedModule
+  ]
 })
 export class CoreModule { }
